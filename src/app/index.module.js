9a88d5dc6cf80c52136363cd23f4496a55051f9a
Diêgo Bolina <diego.bolina@gmail.com>
2016-06-22 00:52:12 -0300

    Add angular-multi-steps-form in modules

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('testMockup', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ngMaterial', 'toastr']);
+    .module('testMockup', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ngMaterial', 'toastr', 'multiStepForm']);
 
 })();
