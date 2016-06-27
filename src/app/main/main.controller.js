5d0db0359726b77e7b0a8ef994050e0d00b30f2d
Diêgo Bolina <diego.bolina@gmail.com>
2016-06-27 19:29:10 -0300

    Organize Controller functions

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,58 +3,82 @@
 
   angular
     .module('testMockup')
-    .controller('MainController', ['$scope', '$http', function($scope, $http) {
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController($scope, $http) {
         var vm = $scope;
-            vm.model = {};
-            vm.formStarWars = {};
-            vm.formGameOfThrones = {};
-            vm.formFood = {};
-            vm.steps = [
-                {
-                    templateUrl: '/app/step1/step1.html',
-                    hasForm: true,
-                },
-                {
-                    templateUrl: '/app/step2/step2.html',
-                    hasForm: true,
-                },
-                {
-                    templateUrl: '/app/step3/step3.html',
-                    hasForm: true,
-                },
-                {
-                    templateUrl: '/app/step4/step4.html',
-                    hasForm: true,
-                },
-                {
-                    templateUrl: '/app/result/result.html',
-                }
-            ];
+        vm.model = {};
+        vm.formStarWars = {};
+        vm.formGameOfThrones = {};
+        vm.formFood = {};
+        vm.steps = [
+            {
+                templateUrl: '/app/step1/step1.html',
+                hasForm: true
+            },
+            {
+                templateUrl: '/app/step2/step2.html',
+                hasForm: true
+            },
+            {
+                templateUrl: '/app/step3/step3.html',
+                hasForm: true
+            },
+            {
+                templateUrl: '/app/step4/step4.html',
+                hasForm: true
+            },
+            {
+                templateUrl: '/app/result/result.html'
+            }
+        ];
 
-           vm.getStarWars = function(){
-                $http.get("http://www.omdbapi.com/?s=Star%20Wars:%20Episode")
-                .then(function(response) {
-                    vm.dataStarWars = response.data;
-                });
-           } 
+        function getStarWars() {
+          $http.get("http://www.omdbapi.com/?s=Star%20Wars:%20Episode")
+          .then(function(response) {
+              vm.dataStarWars = response.data;
+          });
+        }
 
-           vm.getGameOfThrones = function(){
-                $http.get("http://www.omdbapi.com/?t=Game%20of%20Thrones")
-                .then(function(response) {
-                    vm.dataGameOfThrones = response.data;
-                });
-           } 
+        function getGameOfThrones() {
+          $http.get("http://www.omdbapi.com/?t=Game%20of%20Thrones")
+          .then(function(response) {
+              vm.dataGameOfThrones = response.data;
+          });
+        }
 
-           vm.getGameOfThronesSeason = function(season){
-                $http.get("http://www.omdbapi.com/?t=Game%20of%20Thrones&Season=" + season)
-                .then(function(response) {
-                    vm.dataGameOfThronesSeason = response.data;
-                    vm.formGameOfThrones = response.data;
-                }); 
-           } 
+        vm.getGameOfThronesSeason = function(season){
+              $http.get("http://www.omdbapi.com/?t=Game%20of%20Thrones&Season=" + season)
+              .then(function(response) {
+                  vm.dataGameOfThronesSeason = response.data;
+                  vm.formGameOfThrones = response.data;
+              }); 
+         } 
 
-           vm.getStarWars();
-           vm.getGameOfThrones();
-           // vm.getGameOfThronesSeason(2);
-    }]);
+        getStarWars();
+        getGameOfThrones();
+
+    // activate();
+
+    // function activate() {
+    //   getWebDevTec();
+    //   $timeout(function() {
+    //     vm.classAnimation = 'rubberBand';
+    //   }, 4000);
+    // }
+
+    // function showToastr() {
+    //   toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
+    //   vm.classAnimation = '';
+    // }
+
+    // function getWebDevTec() {
+    //   vm.awesomeThings = webDevTec.getTec();
+
+    //   angular.forEach(vm.awesomeThings, function(awesomeThing) {
+    //     awesomeThing.rank = Math.random();
+    //   });
+    // }
+  }
 })();
