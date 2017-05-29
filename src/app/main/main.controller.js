a0bf8b459eec479199cc21b744dd493e0ab83774
Diego Bolina <diego.bolina@gmail.com>
2017-05-29 06:20:20 -0300

    Add API key in omdb urls

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -9,6 +9,7 @@
   function MainController($scope, $http) {
         var vm = $scope;
         vm.model = {};
+        vm.radioQuestion = {};
         vm.formStarWars = {};
         vm.formGameOfThrones = {};
         vm.formFood = {};
@@ -35,23 +36,22 @@
         ];
 
         function getStarWars() {
-          $http.get("http://www.omdbapi.com/?s=Star%20Wars:%20Episode")
+          $http.get("http://www.omdbapi.com/?apikey=bd9be61b&s=Star%20Wars:%20Episode")
           .then(function(response) {
               vm.dataStarWars = response.data;
           });
         }
 
         function getGameOfThrones() {
-          $http.get("http://www.omdbapi.com/?t=Game%20of%20Thrones")
+          $http.get("http://www.omdbapi.com/?apikey=bd9be61b&t=Game%20of%20Thrones")
           .then(function(response) {
               vm.dataGameOfThrones = response.data;
           });
         }
 
         vm.getGameOfThronesSeason = function(season){
-              $http.get("http://www.omdbapi.com/?t=Game%20of%20Thrones&Season=" + season)
+              $http.get("http://www.omdbapi.com/?apikey=bd9be61b&t=Game%20of%20Thrones&Season=" + season)
               .then(function(response) {
-                  vm.dataGameOfThronesSeason = response.data;
                   vm.formGameOfThrones = response.data;
               }); 
          } 
